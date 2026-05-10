4e2cd53f2f3163a286eef7610f17470b20badffb
Jovansa Putra Laksana <jovansaputralaksana@gmail.com>
2026-05-10 19:48:24 +0700

    belajar buffered channel

--- channel_test.go
+++ channel_test.go
@@ -18,3 +18,59 @@ func TestCreateChannel(t *testing.T) {
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeresponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Jovansa Putra Laksana"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeresponse(channel)
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Hello World"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+	time.Sleep(5 * time.Second)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Jovansa"
+		channel <- "Putra"
+		channel <- "Laksana"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Selesai")
+}
