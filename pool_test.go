2a2cc66cbb2e1671a7227ab4db7c5cf0fd37f95f
Jovansa Putra Laksana <jovansaputralaksana@gmail.com>
2026-05-12 14:50:12 +0700

    pool

---
+++ pool_test.go
@@ -0,0 +1,32 @@
+package belajar_golang_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	pool := sync.Pool{
+		New: func() interface{} {
+			return "New"
+		},
+	}
+
+	pool.Put("Jovansa")
+	pool.Put("Putra")
+	pool.Put("Laksana")
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			time.Sleep(1 * time.Second)
+			pool.Put(data)
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Selesai")
+}
